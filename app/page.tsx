98c92aa0654281da9f37fddd762cfbdced57f745
krishnoit <krishnoit100@gmail.com>
2023-06-29 19:44:13 +0530

    FEAT :card section done

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,40 @@
 
 import React from "react";
 import Navbar from "./utlis/navabar";
+import CardThree from "./components/cards";
 
 export default function Index() {
-  return <Navbar />;
+  return (
+    <>
+      <Navbar />
+      <div className="flex justify-center items-center ">
+        <div>
+          <div className="font-semibold py-8 underline underline-offset-1">
+            <h1>Category 1</h1>
+          </div>
+          <div className="flex grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-4 justify-center items-center">
+            <CardThree />
+            <CardThree />
+            <CardThree />
+          </div>
+          <div className="font-semibold py-8 underline underline-offset-1">
+            <h1>Category 2</h1>
+          </div>
+          <div className="flex grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-4 justify-center items-center">
+            <CardThree />
+            <CardThree />
+            <CardThree />
+          </div>
+          <div className="font-semibold py-8 underline underline-offset-1">
+            <h1>Category 3</h1>
+          </div>
+          <div className="flex grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-4 justify-center items-center">
+            <CardThree />
+            <CardThree />
+            <CardThree />
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
