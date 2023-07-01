c6f8a12a1eed12f829b72cf8698a0727785185be
shivamsinghrajputcse <shivamsinghcse21@gmail.com>
2023-07-01 21:38:18 +0530

    adding cart buy backbutton

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -2,6 +2,8 @@
 import React, { useState } from 'react'
 import { ArrowRight } from 'lucide-react'
 import Link from 'next/link'
+import Image from 'next/image';
+import backbuttonimg from '../icons/backbutton.png'
 
 export default function SignInOne() {
   const [email, setEmail] = useState<string>('')
@@ -41,14 +43,15 @@ export default function SignInOne() {
 
   return (
     <section>
-      {/* <button
-            type="button"
-            className="mt-4 w-full rounded-md bg-black px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-black/80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black"
-          >
-            <Link href="/signup/"><img src=></img></Link>
-          </button> */}
+      
       <div className="flex items-center justify-center px-4 py-10 sm:px-6 sm:py-16 lg:px-8 lg:py-24">
+      
         <div className="xl:mx-auto xl:w-full xl:max-w-sm 2xl:max-w-md">
+        <Link href="/">
+        <button>
+          <Image src={backbuttonimg} alt='Go Back' className='w-4 md:w-7 lg:7'/>
+        </button>
+      </Link>
           <div className="mb-2 flex justify-center">
             <svg
               width="50"
